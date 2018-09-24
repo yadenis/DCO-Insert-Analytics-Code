284189c0b75f64e92889d4f813ee6c1d7d6e8cd9
Denis Yanchevskiy <denis@denisco.pro>
2018-09-24 15:40:07 +0300

    Corrected for WordPress Coding Standards

--- dco-insert-analytics-code.php
+++ dco-insert-analytics-code.php
@@ -23,8 +23,8 @@ define( 'DCO_IAC__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'DCO_IAC__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'DCO_IAC__PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
-require_once DCO_IAC__PLUGIN_DIR . 'class.dco-iac-base.php';
-require_once DCO_IAC__PLUGIN_DIR . 'class.dco-iac.php';
+require_once DCO_IAC__PLUGIN_DIR . 'class-dco-iac-base.php';
+require_once DCO_IAC__PLUGIN_DIR . 'class-dco-iac.php';
 if ( is_admin() ) {
-	require_once DCO_IAC__PLUGIN_DIR . 'class.dco-iac-admin.php';
+	require_once DCO_IAC__PLUGIN_DIR . 'class-dco-iac-admin.php';
 }
